f2c8e6f8fbcf2723b8e5fa653a8af8009c0cfb93
Philipp <if25b147@technikum-wien.at>
2026-05-18 21:00:52 +0200

    Working code for todo6 (correct count when x and y are the same)

--- help_alf/src/help_alf.js
+++ help_alf/src/help_alf.js
@@ -1,1 +1,16 @@
+function findSpaceship(map) {
 
+    const rows = map.split("\n");
+
+    for (let i = 0; i < rows.length; i++) {
+
+        const x = rows[i].indexOf("X");
+
+        if (x !== -1) {
+
+            const y = rows.length - 1 - i;
+
+                return [x, y,];
+        }
+    }
+}
